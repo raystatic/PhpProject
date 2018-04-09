e5f95efde735f6611c1c3f1ebf7d7e8bd6e18e9c
rahul <rahul9650ray@gmail.com>
2018-04-10 00:30:06 +0530

    Chat functionality modified

--- app/src/main/java/com/example/rahul/phpproject/Activities/Chat3Activity.java
+++ app/src/main/java/com/example/rahul/phpproject/Activities/Chat3Activity.java
@@ -20,12 +20,15 @@ import com.example.rahul.phpproject.Adapters.ChatAdapter;
 import com.example.rahul.phpproject.Interfaces.ChatInterface;
 import com.example.rahul.phpproject.R;
 import com.example.rahul.phpproject.background.Chat;
+import com.example.rahul.phpproject.background.GetChats;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -68,10 +71,43 @@ public class Chat3Activity extends AppCompatActivity{
         recievers=new ArrayList<>();
         msgs=new ArrayList<>();
 
+        info.setVisibility(View.GONE);
+
+
         sharedPreferences=getSharedPreferences("users",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
 
         myEmail=sharedPreferences.getString("email","rahul");
+
+        GetChats getChats=new GetChats(Chat3Activity.this, new ChatInterface() {
+            @Override
+            public void onChatFinishes(ArrayList<String> senders, ArrayList<String> reciever, ArrayList<String> msgs) {
+                LinearLayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
+                ChatAdapter adapter=new ChatAdapter(getApplicationContext(),senders,reciever,msgs, friendEmail, myEmail);
+                recyclerView.setLayoutManager(layoutManager);
+                recyclerView.setAdapter(adapter);
+            }
+        });
+
+
+        getChats.execute(myEmail,friendEmail);
+
+        Timer timer=new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                GetChats getChats=new GetChats(Chat3Activity.this, new ChatInterface() {
+                    @Override
+                    public void onChatFinishes(ArrayList<String> senders, ArrayList<String> reciever, ArrayList<String> msgs) {
+                        LinearLayoutManager layoutManager=new LinearLayoutManager(getApplicationContext());
+                        ChatAdapter adapter=new ChatAdapter(getApplicationContext(),senders,reciever,msgs, friendEmail, myEmail);
+                        recyclerView.setLayoutManager(layoutManager);
+                        recyclerView.setAdapter(adapter);
+                    }
+                });
+                getChats.execute(myEmail,friendEmail);
+            }
+        },5000,1000);
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,7 +120,6 @@ public class Chat3Activity extends AppCompatActivity{
                 }
                 else
                 {
-                    info.setVisibility(View.GONE);
 
                     try {
                        // ChatInterface chatInterface=null;
